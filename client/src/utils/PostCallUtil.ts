f5da8a398a85a06e46b9e3ac2ae4f75d48caad0d
samynasry2020 <87795536+samynasry2020@users.noreply.github.com>
2024-01-30 12:03:05 -0800

    Initialize tejaswiputhi/WorflowOSUpdate

---
+++ client/src/utils/PostCallUtil.ts
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+export const submitSurveyResponseUtil = async (
+  acsUserId: string,
+  pollResponse: number | boolean | string | undefined,
+  meetingLink: string,
+  callId?: string
+): Promise<void> => {
+  try {
+    const res = await fetch('/api/surveyResults', {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      mode: 'cors',
+      method: 'POST',
+      body: JSON.stringify({
+        callId: callId,
+        acsUserId: acsUserId,
+        meetingLink: meetingLink,
+        response: pollResponse
+      })
+    });
+    if (res.status !== 200) {
+      throw new Error('Error during insertion');
+    }
+  } catch (e) {
+    //todo - Error logging;
+  }
+};
