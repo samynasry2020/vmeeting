0ddcb82a9537fcf0c919faffc19925f4b03a8374
samynasry2020 <87795536+samynasry2020@users.noreply.github.com>
2024-01-30 12:03:00 -0800

    Initialize hansung/one-question-poll-api

---
+++ client/src/styles/Survey.styles.ts
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+export const surveyStyle = {
+  root: { height: '100%' }
+};
+
+export const rejoinLinkStyle = { root: { height: '5rem', textDecoration: 'underline' } };
+
+export const surveyIframeStyle = { height: '100%', width: '100%', border: 0 };
